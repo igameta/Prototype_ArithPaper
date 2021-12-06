58ed8b7628a73edd453ecdd6332b8b5880d3ea69
TakanakaIme <igameta0510@outlook.jp>
2021-12-06 16:59:58 +0900

    Tool wip
    made CurrentToolNavigator but cannot work well

---
+++ Unity/Assets/Scripts/Common/CurToolLocationChanger.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurToolLocationChanger : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+        pos.x=-305; pos.y = 191; pos.z = 0;
+        this.gameObject.transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int CurTool = FreeDraw.ToolSelection.GetUsingTool();
+        Vector3 pos = this.gameObject.transform.position;
+        pos.y = 191; pos.z = 0;
+
+        switch (CurTool)
+        {
+            case 0:
+                pos.x=-305;
+                this.gameObject.transform.position = pos;
+                break;
+
+            case 1:
+                pos.x=-249;
+                this.gameObject.transform.position = pos;
+                break;
+
+            case 2:
+                pos.x=-192;
+                this.gameObject.transform.position = pos;
+                break;
+
+            case 3:
+                pos.x=252;
+                this.gameObject.transform.position = pos;
+                break;
+
+        }
+    }
+}
--- Unity/Assets/Scripts/Common/ToolSelection.cs
+++ Unity/Assets/Scripts/Common/ToolSelection.cs
@@ -1,68 +1,137 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ToolSelection : MonoBehaviour
+
+namespace FreeDraw
 {
-    /* 選択中のツールを保持 初期値:0(Pen)
-               Pen=0   Highrighter=1 
-            Eraser=2 Constractions=3
-    */
-    public enum Toolmode
+    public class ToolSelection : MonoBehaviour
     {
-        Pen,
-        Highlighter,
-        Eraser,
-        Constractions
-    }
-    public Toolmode UsingTool = Toolmode.Pen;
-
-    // 選択中の色の保持
-    public enum ColorPicker
-    {
-        first,
-        second,
-        third
-    }
-    public ColorPicker CurColor = ColorPicker.first;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-
-    // ツールが選択されたときの処理
-    public void ChangeTool ()
-    {
-        switch (UsingTool)
+        /* 選択中のツールを保持 初期値:0(Pen)
+                   Pen=0   Highrighter=1 
+                Eraser=2 Constractions=3
+        */
+        public enum Toolmode
         {
-            case Toolmode.Pen:
-                break;
+            Pen=0,
+            Highlighter=1,
+            Eraser=2,
+            Constractions=3
+        }
+        public static Toolmode UsingTool = Toolmode.Pen;
 
-            case Toolmode.Highlighter:
-                break;
+        // UsingToolGeter
+        public static int GetUsingTool()
+        {
+            return (int)UsingTool;
+        }
 
-            case Toolmode.Eraser:
-                break;
+        // 選択中の色の保持
+        public enum ColorPicker
+        {
+            first,
+            second,
+            third
+        }
+        public ColorPicker CurColor = ColorPicker.first;
 
-            case Toolmode.Constractions:
-                break;
+        // 選択ツールを有効化
+        private void ToolActivator ( Toolmode SelTool )
+        {
+            switch (SelTool)
+            {
+                case Toolmode.Pen:
+                    UsingTool = Toolmode.Pen;
+                    //CurrentTool.transform.Translate(-305, 191, 0);
+                    break;
+
+                case Toolmode.Highlighter:
+                    UsingTool = Toolmode.Highlighter;
+                    //CurrentTool.transform.Translate(-249, 191, 0);
+                    break;
+
+                case Toolmode.Eraser:
+                    UsingTool = Toolmode.Eraser;
+                    //CurrentTool.transform.Translate(-192, 191, 0);
+                    break;
+
+                case Toolmode.Constractions:
+                    UsingTool = Toolmode.Constractions;
+                    //CurrentTool.transform.Translate(252, 191, 0);
+                    break;
+
+            }
+        }
+
+        //// 選択カラーピッカーを有効化
+        //void ColorActivator(ColorPicker SelColor)
+        //{
+
+        //}
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //念の為初期化
+            UsingTool = Toolmode.Pen;
+            CurColor = ColorPicker.first;
+
+            // ボタン押下検知
+            //Button btn = ToolSelection.GetComponent<Button>();
+            //btn.onClick.AddListener(TaskOnClick);
+        }
+
+
+        // ツールが選択されたときの処理
+        public void ChangeTool( Toolmode SelTool )
+        {
+            // 選択ツールが選択中のツールと同じか判定
+            if ( SelTool != UsingTool)
+            {
+                // ToolActivatorの起動とか
+                switch (SelTool)
+                {
+                    case Toolmode.Pen:
+                        ToolActivator(SelTool);
+                    break;
+
+                    case Toolmode.Highlighter:
+                        ToolActivator(SelTool);
+                    break;
+
+                    case Toolmode.Eraser:
+                        ToolActivator(SelTool);
+                    break;
+
+                    case Toolmode.Constractions:
+                        ToolActivator(SelTool);
+                    break;
+
+                }
+            }
+            // 太さの変更処理
+            else if ( SelTool != Toolmode.Constractions)
+            {
+
+            }
+        }
+
+        // カラーピッカーを押されたときの処理
+        //public void ChangeColorPicker ( ColorPicker SelPicker )
+        //{
+        //    if ( CurColor != SelPicker)
+        //    {
+
+        //    }
+        //}
+
+        // Update is called once per frame
+        void Update()
+        {
 
         }
-    }
 
-    // カラーピッカーを押されたときの処理
-    public void ChangeColorPicker
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
 }
